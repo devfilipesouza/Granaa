23374b0a4745471545eb611906dc08d4802f81f9
Filipe Souza <filipe.svito@gmail.com>
2025-06-16 20:10:52 -0300

    Update Spacing.js

--- src/constants/Spacing.js
+++ src/constants/Spacing.js
@@ -6,8 +6,8 @@ export default {
   extraLarge: 32,
   huge: 48,
   screenPadding: 20,
-  cardPadding: 25, // Novo para o padding interno do card de despesas
-  inputPaddingVertical: 10, // Para padding de inputs
-  inputBottomMargin: 20, // Para margin inferior de inputs e dropdowns
-  buttonPaddingVertical: 14, // Para padding de botões
+  cardPadding: 25,
+  inputPaddingVertical: 10,
+  inputBottomMargin: 20,
+  buttonPaddingVertical: 14,
 };
